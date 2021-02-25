ec4a72bccb37679f9060abc2e0702dca1f31ae3e
Torenia.Founieri <torenia.fournieri@gmail.com>
2021-02-25 23:21:56 +0900

    Counter, global counter

--- TestBattle.py
+++ TestBattle.py
@@ -51,8 +51,15 @@ Pig_ActiveMove2 = MoveClass("回復", True,0,0,0,0,"None", +3, "緑", "Self", "N
 1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
 Pig_ActiveMove3 = MoveClass("攻撃大", True,0,0,0,0,"None", -5, "赤", "Opponent", "None",
 1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_PassiveRedCounterAttack = MoveClass("反撃", False,1,0,0,0,"Opponent", -5, "赤", "Opponent", "Any",
+Pig_RedCounter1 = MoveClass("反撃<赤>", False,1,0,0,0,"Opponent", -5, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+Pig_RedCounter2 = MoveClass("反撃強<赤赤>", False,2,0,0,0,"Opponent", -7, "赤", "Opponent", "Any",
+0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+Pig_BlueCounter1 = MoveClass("防御削り<黄>", False,0,0,1,0,"Global", -1, "青", "Opponent", "Any",
+0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+
+Pig_Counters = [Pig_RedCounter1, Pig_RedCounter2, Pig_BlueCounter1]
+
 Pig_PassiveRedReAttack = MoveClass("再攻撃", False,1,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.3, True, 20, 0,0,0,0,0,0,0,0,0,0,0 )
 
@@ -62,10 +69,19 @@ Elder_ActiveMove2 = MoveClass("防御", True,0,0,0,0,"none", 0, "黄", "Self", "
 1.0, False, 20, 0,0,0,0,3,0,0,0,0,0,0 )
 Elder_ActiveMove3 = MoveClass("攻撃大", True,0,0,0,0,"none", -5, "赤", "Opponent", "None",
 1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Elder_PassiveRedCounterAttack = MoveClass("反撃", False,1,0,0,0,"Opponent", -5, "赤", "Opponent", "Any",
+Elder_RedCounter1 = MoveClass("反撃<赤>", False,1,0,0,0,"Opponent", -5, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+Elder_GreenCounter1 = MoveClass("回復潰し<青>", False,0,0,0,1,"Global", -2, "青", "Opponent", "Any",
+0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+Elder_BlueCounter1 = MoveClass("追加介入<青黄>", False,0,1,1,0,"Opponent", -3, "青", "Opponent", "Any",
+0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+
+Elder_Counters = [Elder_RedCounter1, Elder_GreenCounter1, Elder_BlueCounter1]
+
 Elder_PassiveRedReAttack = MoveClass("再攻撃", False,1,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.3, True, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+
+
 
 class CharacterClass:
     isAlly = False
@@ -76,7 +92,7 @@ class CharacterClass:
 
     def __init__(self, name, isAlly:bool, hp:int,
      resistRed:int, resistBlue:int, resistYellow:int, resistGreen:int,
-     activeMove1,activeMove2,activeMove3, passiveReAttack, passiveCounterAttack):
+     activeMove1,activeMove2,activeMove3, passiveReAttack, counters):
         self.name = name
         self.isAlly = isAlly
         self.maxHp = hp
@@ -89,14 +105,14 @@ class CharacterClass:
         self.activeMove2 = activeMove2 # Temp
         self.activeMove3 = activeMove3 # Temp
         self.passiveReAttack = passiveReAttack # Temp; it should be arry.
-        self.passiveCounterAttack = passiveCounterAttack # Temp; it should be arry.
+        self.counters = counters # Temp; it should be arry.
         #self.currentMove = activeSlots # temp; it is ugly.
 
 # Cast CharacterClass
 Ally1 = CharacterClass("ピグ", True, 20, 1, 0, 0, 0, Pig_ActiveMove1,Pig_ActiveMove2,Pig_ActiveMove3,
- Pig_PassiveRedReAttack, Pig_PassiveRedCounterAttack)
+ Pig_PassiveRedReAttack, Pig_Counters)
 Enemy1 = CharacterClass("エルダー", False, 30, 0, 0, 0, 0, Elder_ActiveMove1, Elder_ActiveMove2, Elder_ActiveMove3,
- Elder_PassiveRedReAttack, Elder_PassiveRedCounterAttack)
+ Elder_PassiveRedReAttack, Elder_Counters)
 
 class MoveOrderClass:
     def __init__(self, actor, currentMove, chainCount:int, isInitialMove:bool):
@@ -122,7 +138,7 @@ battleCount = 0
 winCount = 0
 drawCount = 0
 
-while (battleCount < 1):
+while (battleCount < 100):
     # setup, clean
     current_turn = 1
     for character in character_list:
@@ -139,7 +155,8 @@ while (battleCount < 1):
 
         ## Clear up chain element in Turn phase
         for character in character_list:
-            character.passiveCounterAttack.canMoveInThisTurn = True
+            for counter in character.counters:
+                counter.canMoveInThisTurn = True
             character.passiveReAttack.canMoveInThisTurn = True
 
         ##[2] Move Order calculation
@@ -248,7 +265,7 @@ while (battleCount < 1):
             #debugText = " (" + str(turnChainCount.chainRedStack) + "/" + str(turnChainCount.chainBlueStack)+ "/" + str(turnChainCount.chainYellowStack)+ "/" + str(turnChainCount.chainGreenStack) + ")"
 
             print("  "*actorOrder.chainCount + elementText + " -"+actorOrder.currentMove.name + "- " + actorOrder.actor.name
-            + " -> " + target.name + "  " + battleText + buffText  )
+            + " -> " + target.name + "  " + battleText + buffText )
 
             #[XX] Result evaluation
             if target.currentHp <= 0:
@@ -267,32 +284,60 @@ while (battleCount < 1):
 
             #[3-1] Chain Move evaluation
             while True:
-                # Priority: (1)Opponent reaction -> (2)Self reaction
+                # Priority: (1)Opponent reaction -> (2)Global reaction -> (3)Self reaction
                 # (1) Opponent reaction
                 # chainReference = Opponent should work
-                if(target.passiveCounterAttack.canMoveInThisTurn and target.passiveCounterAttack.isActiveMove == False
-                and target.passiveCounterAttack.chainReference == "Opponent"
-                and target.passiveCounterAttack.chainTriggerElementRed <= turnChainCount.chainRedStack
-                and target.passiveCounterAttack.chainTriggerElementBlue <= turnChainCount.chainBlueStack
-                and target.passiveCounterAttack.chainTriggerElementYellow <= turnChainCount.chainYellowStack
-                and target.passiveCounterAttack.chainTriggerElementGreen <= turnChainCount.chainGreenStack
-                ):
-                    if(target.passiveCounterAttack.invocationRate >= random.uniform(0.0, 1.0) ):
-                        if(target.passiveCounterAttack.canTriggerMultipleInOneTurn == False):
-                            target.passiveCounterAttack.canMoveInThisTurn = False
-                        reaction = MoveOrderClass(target, target.passiveCounterAttack, actorOrder.chainCount +1, False)
-                        actionOrderList.insert(0,reaction)
-                        # print("反撃　発動: " + reaction.actor.name + " chainCount:" + str(reaction.chainCount))
-                        break
+                for counter in target.counters:
+                    flag = False
+                    if(counter.canMoveInThisTurn and counter.isActiveMove == False
+                    and counter.chainReference == "Opponent"
+                    and counter.chainTriggerElementRed <= turnChainCount.chainRedStack
+                    and counter.chainTriggerElementBlue <= turnChainCount.chainBlueStack
+                    and counter.chainTriggerElementYellow <= turnChainCount.chainYellowStack
+                    and counter.chainTriggerElementGreen <= turnChainCount.chainGreenStack
+                    ):
+                        if(counter.invocationRate >= random.uniform(0.0, 1.0) ):
+                            if(counter.canTriggerMultipleInOneTurn == False):
+                                counter.canMoveInThisTurn = False
+                            reaction = MoveOrderClass(target, counter, actorOrder.chainCount +1, False)
+                            actionOrderList.insert(0,reaction)
+                            # print("反撃　発動: " + reaction.actor.name + " chainCount:" + str(reaction.chainCount))
+                            flag = True
+                            break
+                if(flag):
+                    break
 
-                # (2)Self reaction
+                # (2) Global reaciton
+                for character in character_list:
+                    flag = False
+                    for counter in character.counters:
+                        if(counter.canMoveInThisTurn and counter.isActiveMove == False
+                        and counter.chainReference == "Global"
+                        and counter.chainTriggerElementRed <= turnChainCount.chainRedStack
+                        and counter.chainTriggerElementBlue <= turnChainCount.chainBlueStack
+                        and counter.chainTriggerElementYellow <= turnChainCount.chainYellowStack
+                        and counter.chainTriggerElementGreen <= turnChainCount.chainGreenStack
+                        ):
+                            if(counter.invocationRate >= random.uniform(0.0, 1.0) ):
+                                if(counter.canTriggerMultipleInOneTurn == False):
+                                    counter.canMoveInThisTurn = False
+                                reaction = MoveOrderClass(character, counter, actorOrder.chainCount +1, False)
+                                actionOrderList.insert(0,reaction)
+                                flag = True
+                                break
+                        if(flag):
+                            break
+                if(flag):
+                    break
+
+                # (3)Self reaction
                 # chainReference = Self should work
                 if(actorCharacter.passiveReAttack.canMoveInThisTurn and actorCharacter.passiveReAttack.isActiveMove == False
                 and actorCharacter.passiveReAttack.chainReference == "Self"
-                and target.passiveCounterAttack.chainTriggerElementRed <= turnChainCount.chainRedStack
-                and target.passiveCounterAttack.chainTriggerElementBlue <= turnChainCount.chainBlueStack
-                and target.passiveCounterAttack.chainTriggerElementYellow <= turnChainCount.chainYellowStack
-                and target.passiveCounterAttack.chainTriggerElementGreen <= turnChainCount.chainGreenStack
+                and target.passiveReAttack.chainTriggerElementRed <= turnChainCount.chainRedStack
+                and target.passiveReAttack.chainTriggerElementBlue <= turnChainCount.chainBlueStack
+                and target.passiveReAttack.chainTriggerElementYellow <= turnChainCount.chainYellowStack
+                and target.passiveReAttack.chainTriggerElementGreen <= turnChainCount.chainGreenStack
                 ):
                     if(target.passiveReAttack.canTriggerMultipleInOneTurn == False):
                         target.passiveReAttack.canMoveInThisTurn = False
