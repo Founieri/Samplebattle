9ac50ef11d19801f09361020691c7899673df114
Torenia.Founieri <torenia.fournieri@gmail.com>
2021-02-27 23:27:45 +0900

    random skill generate

--- TestBattle.py
+++ TestBattle.py
@@ -35,93 +35,333 @@ class MoveClass:
     # In battle info
     inBattleMovedCount = 0
     canMoveInThisTurn = True
-
+    # isActiveMove = False
+    # moveElement = ""
+    # moveValue = 0
     # Preset infomation, Move master.
-    def __init__(self, name, isActiveMove:bool,
-     chainTriggerElementRed:int,chainTriggerElementBlue:int,chainTriggerElementYellow:int,chainTriggerElementGreen:int,
-     chainReference, moveValue:int, moveElement, toTarget, whatTrigger,
-     invocationRate:float, canTriggerMultipleInOneTurn:bool, numberOfPossibleMoves:int,
-     buffOffenceRedStack:int, buffOffenceBlueStack:int, buffOffenceYellowStack:int, buffOffenceGreenStack:int,
-     buffDefenceRedStack:int, buffDefenceBlueStack:int, buffDefenceYellowStack:int, buffDefenceGreenStack:int,
-     buffAccuracyStack:int, buffEvasionStack:int, buffSpeedStack:int
+    def __init__(self,isNull:bool, name = "", isActiveMove:bool = False,
+     chainTriggerElementRed:int = 0,chainTriggerElementBlue:int = 0,chainTriggerElementYellow:int = 0,chainTriggerElementGreen:int = 0,
+     chainReference = "None", moveValue:int = 1, moveElement = "赤", toTarget ="Opponent", whatTrigger= "any",
+     invocationRate:float=0.10, canTriggerMultipleInOneTurn:bool=False, numberOfPossibleMoves:int=0,
+     buffOffenceRedStack:int=0, buffOffenceBlueStack:int=0, buffOffenceYellowStack:int=0, buffOffenceGreenStack:int=0,
+     buffDefenceRedStack:int=0, buffDefenceBlueStack:int=0, buffDefenceYellowStack:int=0, buffDefenceGreenStack:int=0,
+     buffAccuracyStack:int=0, buffEvasionStack:int=0, buffSpeedStack:int=0
      ):
-        self.name = name # Name of move
-        self.isActiveMove = isActiveMove    # true:"active" or false:"passive"
-        self.chainTriggerElementRed = chainTriggerElementRed
-        self.chainTriggerElementBlue = chainTriggerElementBlue
-        self.chainTriggerElementYellow = chainTriggerElementYellow
-        self.chainTriggerElementGreen = chainTriggerElementGreen
-        self.chainReference = chainReference # whitch move type trigger this move in reaction
-        self.moveValue = moveValue # 0: non offence nor heal move. >= +1: heal toTarget. <= -1: attack toTarget.
-        self.moveElement = moveElement # 赤, 青, 黄, 緑
-        self.toTarget = toTarget # "Self" or "Opponent"  note: now only limited 1 vs 1.
-        self.whatTrigger = whatTrigger #
-        self.invocationRate = invocationRate  # 0.0 ~ 1.0
-        self.canTriggerMultipleInOneTurn = canTriggerMultipleInOneTurn  # boolean
-        self.numberOfPossibleMoves = numberOfPossibleMoves
-        self.buffOffenceRedStack = buffOffenceRedStack
-        self.buffOffenceBlueStack = buffOffenceBlueStack
-        self.buffOffenceYellowStack = buffOffenceYellowStack
-        self.buffOffenceGreenStack = buffOffenceGreenStack
-        self.buffDefenceRedStack = buffDefenceRedStack
-        self.buffDefenceBlueStack = buffDefenceBlueStack
-        self.buffDefenceYellowStack = buffDefenceYellowStack
-        self.buffDefenceGreenStack = buffDefenceGreenStack
-        self.buffAccuracyStack = buffAccuracyStack
-        self.buffEvasionStack = buffEvasionStack
-        self.buffSpeedStack = buffSpeedStack
+        if(isNull):
+            for p in range(25):
+                self.geneticMutate(p)
+            isActiveText = ""
+            if(self.isActiveMove):
+                isActiveText = "通"
+            else:
+                isActiveText = "反"
+            self.name = isActiveText + "[" + str(self.moveValue) + str(self.moveElement) + "]"
 
+        else:
+            self.name = name # Name of move
+            self.isActiveMove = isActiveMove    # true:"active" or false:"passive"
+            self.chainTriggerElementRed = chainTriggerElementRed
+            self.chainTriggerElementBlue = chainTriggerElementBlue
+            self.chainTriggerElementYellow = chainTriggerElementYellow
+            self.chainTriggerElementGreen = chainTriggerElementGreen
+            self.chainReference = chainReference # whitch move type trigger this move in reaction
+            self.moveValue = moveValue # 0: non offence nor heal move. >= +1: heal toTarget. <= -1: attack toTarget.
+            self.moveElement = moveElement # 赤, 青, 黄, 緑
+            self.toTarget = toTarget # "Self" or "Opponent"  note: now only limited 1 vs 1.
+            self.whatTrigger = whatTrigger #
+            self.invocationRate = invocationRate  # 0.0 ~ 1.0
+            self.canTriggerMultipleInOneTurn = canTriggerMultipleInOneTurn  # boolean
+            self.numberOfPossibleMoves = numberOfPossibleMoves
+            self.buffOffenceRedStack = buffOffenceRedStack
+            self.buffOffenceBlueStack = buffOffenceBlueStack
+            self.buffOffenceYellowStack = buffOffenceYellowStack
+            self.buffOffenceGreenStack = buffOffenceGreenStack
+            self.buffDefenceRedStack = buffDefenceRedStack
+            self.buffDefenceBlueStack = buffDefenceBlueStack
+            self.buffDefenceYellowStack = buffDefenceYellowStack
+            self.buffDefenceGreenStack = buffDefenceGreenStack
+            self.buffAccuracyStack = buffAccuracyStack
+            self.buffEvasionStack = buffEvasionStack
+            self.buffSpeedStack = buffSpeedStack
+
+
+
+    def geneticMutate(self,position:int):
+        if (position == 1):
+            try:
+                if (self.isActiveMove):
+                    self.isActiveMove = False
+                else:
+                    self.isActiveMove = True
+            except AttributeError as error:
+                self.isActiveMove = False
+        elif (position == 2):
+            try:
+                if(random.randint(0,1) == 1):
+                    self.chainTriggerElementRed += 1
+                else:
+                    self.chainTriggerElementRed -= 1
+            except AttributeError as error:
+                self.chainTriggerElementRed = 0
+        elif(position == 3):
+            try:
+                if(random.randint(0,1) ==1):
+                    self.chainTriggerElementBlue += 1
+                else:
+                    self.chainTriggerElementBlue -= 1
+            except AttributeError as error:
+                self.chainTriggerElementBlue = 0
+        elif(position == 4):
+            try:
+                if(random.randint(0,1) ==1):
+                    self.chainTriggerElementYellow += 1
+                else:
+                    self.chainTriggerElementYellow -= 1
+            except AttributeError as error:
+                self.chainTriggerElementYellow = 0
+        elif(position == 5):
+            try:
+                if(random.randint(0,1) ==1):
+                    self.chainTriggerElementGreen += 1
+                else:
+                    self.chainTriggerElementGreen -= 1
+            except AttributeError as error:
+                self.chainTriggerElementGreen = 0
+
+        elif(position == 6):
+            r = random.randint(0,2)
+            if(r == 0):
+                self.chainReference = "None"
+            elif(r == 1):
+                self.chainReference = "Opponent"
+            elif(r == 2):
+                self.chainReference = "Global"
+        elif(position == 7):
+            try:
+                if(random.randint(0,1) ==1):
+                    self.moveValue += 1
+                else:
+                    self.moveValue -= 1
+            except AttributeError as error:
+                self.moveValue = 0
+
+        elif(position == 8):
+            r = random.randint(0,3)
+            if(r == 0):
+                self.moveElement = "赤"
+            elif(r == 1):
+                self.moveElement = "青"
+            elif(r == 2):
+                self.moveElement = "黄"
+            elif(r == 3):
+                self.moveElement = "緑"
+        elif(position == 9):
+            if(random.randint(0,1) ==1):
+                self.toTarget = "Self"
+            else:
+                self.toTarget = "Opponent"
+        elif(position == 10):
+            r = random.randint(0,1)
+            if(r == 0):
+                self.whatTrigger = "None"
+            else:
+                self.whatTrigger = "Any"
+        elif(position == 11):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.invocationRate += 0.01
+                else:
+                    self.invocationRate -= 0.01
+            except AttributeError as error:
+                self.invocationRate = 0.1
+        elif(position == 12):
+            r = random.randint(0,1)
+            if(r == 0):
+                self.canTriggerMultipleInOneTurn = True
+            else:
+                self.canTriggerMultipleInOneTurn = False
+        elif(position == 13):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.numberOfPossibleMoves += 1
+                else:
+                    self.numberOfPossibleMoves -= 1
+            except AttributeError as error:
+                self.numberOfPossibleMoves = 1
+        elif(position == 14):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffOffenceRedStack += 1
+                else:
+                    self.buffOffenceRedStack -= 1
+            except AttributeError as error:
+                self.buffOffenceRedStack = 0
+
+        elif(position == 15):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffOffenceBlueStack += 1
+                else:
+                    self.buffOffenceBlueStack -= 1
+            except AttributeError as error:
+                self.buffOffenceBlueStack = 0
+        elif(position == 16):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffOffenceYellowStack += 1
+                else:
+                    self.buffOffenceYellowStack -= 1
+            except AttributeError as error:
+                self.buffOffenceYellowStack = 0
+        elif(position == 17):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffOffenceGreenStack += 1
+                else:
+                    self.buffOffenceGreenStack -= 1
+            except AttributeError as error:
+                self.buffOffenceGreenStack = 0
+
+        elif(position == 18):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffDefenceRedStack += 1
+                else:
+                    self.buffDefenceRedStack -= 1
+            except AttributeError as error:
+                self.buffDefenceRedStack = 0
+        elif(position == 19):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffDefenceBlueStack += 1
+                else:
+                    self.buffDefenceBlueStack -= 1
+            except AttributeError as error:
+                self.buffDefenceBlueStack = 0
+        elif(position == 20):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffDefenceYellowStack += 1
+                else:
+                    self.buffDefenceYellowStack -= 1
+            except AttributeError as error:
+                self.buffDefenceYellowStack = 0
+        elif(position == 21):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffDefenceGreenStack += 1
+                else:
+                    self.buffDefenceGreenStack -= 1
+            except AttributeError as error:
+                self.buffDefenceGreenStack = 0
+        elif(position == 22):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffAccuracyStack += 1
+                else:
+                    self.buffAccuracyStack -= 1
+            except AttributeError as error:
+                self.buffAccuracyStack = 1
+
+        elif(position == 23):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffEvasionStack += 1
+                else:
+                    self.buffEvasionStack -= 1
+            except AttributeError as error:
+                self.buffEvasionStack = 0
+        elif(position == 24):
+            try:
+                r = random.randint(0,1)
+                if(r == 0):
+                    self.buffSpeedStack += 1
+                else:
+                    self.buffSpeedStack -= 1
+            except AttributeError as error:
+                self.buffSpeedStack = 0
 
 # Cast MoveClass
-Pig_ActiveMove1 = MoveClass("攻撃中", True,0,0,0,0,"None", -3, "赤", "Opponent", "None",
+Pig_ActiveMove1 = MoveClass(False, "攻撃中", True,0,0,0,0,"None", -3, "赤", "Opponent", "None",
+1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0)
+Pig_ActiveMove2 = MoveClass(False,"回復", True,0,0,0,0,"None", +3, "緑", "Self", "None",
 1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_ActiveMove2 = MoveClass("回復", True,0,0,0,0,"None", +3, "緑", "Self", "None",
+Pig_ActiveMove3 = MoveClass(False,"攻撃大", True,0,0,0,0,"None", -5, "赤", "Opponent", "None",
 1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_ActiveMove3 = MoveClass("攻撃大", True,0,0,0,0,"None", -5, "赤", "Opponent", "None",
-1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_RedCounter1 = MoveClass("反撃<赤>", False,1,0,0,0,"Opponent", -3, "赤", "Opponent", "Any",
+Pig_RedCounter1 = MoveClass(False,"反撃<赤>", False,1,0,0,0,"Opponent", -3, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_RedCounter2 = MoveClass("反撃強<赤赤>", False,2,0,0,0,"Opponent", -5, "赤", "Opponent", "Any",
+Pig_RedCounter2 = MoveClass(False,"反撃強<赤赤>", False,2,0,0,0,"Opponent", -5, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_BlueCounter1 = MoveClass("防御削り<黄>", False,0,0,1,0,"Global", -1, "青", "Opponent", "Any",
+Pig_BlueCounter1 = MoveClass(False,"防御削り<黄>", False,0,0,1,0,"Global", -1, "青", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+
+
 
 Pig_Counters = [Pig_RedCounter1, Pig_RedCounter2, Pig_BlueCounter1]
 
-Pig_Reattack1 = MoveClass("武技雷迅<赤>", False,1,0,0,0,"Self", -2, "赤", "Opponent", "Any",
+
+CsvMove1 = [False,'CsvMove1', False,1,0,0,0,"Self", -2, "赤", "Opponent", "Any",
+0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 ]
+
+CsvMoveSets = [CsvMove1, CsvMove1]
+
+Pig_Reattack1 = MoveClass(False,"武技雷迅<赤>", False,1,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_Reattack2 = MoveClass("武技千烈<赤赤>", False,2,0,0,0,"Self", -2, "赤", "Opponent", "Any",
+Pig_Reattack2 = MoveClass(False,"武技千烈<赤赤>", False,2,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_Reattack3 = MoveClass("武技破岩<赤*3>", False,3,0,0,0,"Self", -2, "赤", "Opponent", "Any",
+Pig_Reattack3 = MoveClass(False,"武技破岩<赤*3>", False,3,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_Reattack4 = MoveClass("武技崩天<赤*4>", False,4,0,0,0,"Self", -2, "赤", "Opponent", "Any",
+Pig_Reattack4 = MoveClass(False,"武技崩天<赤*4>", False,4,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.2, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_Reattack5 = MoveClass("武技天舞<赤*5>", False,5,0,0,0,"Self", -2, "赤", "Opponent", "Any",
+Pig_Reattack5 = MoveClass(False,"武技天舞<赤*5>", False,5,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.2, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_Reattack6 = MoveClass("武技龍迅<赤*6>", False,6,0,0,0,"Self", -3, "赤", "Opponent", "Any",
+Pig_Reattack6 = MoveClass(False,"武技龍迅<赤*6>", False,6,0,0,0,"Self", -3, "赤", "Opponent", "Any",
 0.2, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Pig_Reattack7 = MoveClass("武技虎砲<赤*7>", False,7,0,0,0,"Self", -3, "赤", "Opponent", "Any",
+Pig_Reattack7 = MoveClass(False,"武技虎砲<赤*7>", False,7,0,0,0,"Self", -3, "赤", "Opponent", "Any",
 0.2, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
+
 
 Pig_Reattacks = [Pig_Reattack1, Pig_Reattack2 ,Pig_Reattack3, Pig_Reattack4,
  Pig_Reattack5, Pig_Reattack6, Pig_Reattack7]
 
-Elder_ActiveMove1 = MoveClass("攻撃中", True,0,0,0,0,"None", -3, "赤", "Opponent", "None",
+number = 1;
+for c in CsvMoveSets:
+    move = MoveClass(True)
+
+    Pig_Reattacks.append(move)
+    print(move.name)
+    number += 1
+
+
+
+Elder_ActiveMove1 = MoveClass(False,"攻撃中", True,0,0,0,0,"None", -3, "赤", "Opponent", "None",
 1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Elder_ActiveMove2 = MoveClass("防御", True,0,0,0,0,"none", 0, "黄", "Self", "None",
+Elder_ActiveMove2 = MoveClass(False,"防御", True,0,0,0,0,"none", 0, "黄", "Self", "None",
 1.0, False, 20, 0,0,0,0,3,0,0,0,0,0,0 )
-Elder_ActiveMove3 = MoveClass("攻撃大", True,0,0,0,0,"none", -5, "赤", "Opponent", "None",
+Elder_ActiveMove3 = MoveClass(False,"攻撃大", True,0,0,0,0,"none", -5, "赤", "Opponent", "None",
 1.0, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Elder_RedCounter1 = MoveClass("反撃<赤>", False,1,0,0,0,"Opponent", -3, "赤", "Opponent", "Any",
+Elder_RedCounter1 = MoveClass(False,"反撃<赤>", False,1,0,0,0,"Opponent", -3, "赤", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Elder_GreenCounter1 = MoveClass("回復潰し<青>", False,0,0,0,1,"Global", -2, "青", "Opponent", "Any",
+Elder_GreenCounter1 = MoveClass(False,"回復潰し<青>", False,0,0,0,1,"Global", -2, "青", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
-Elder_BlueCounter1 = MoveClass("追加介入<青黄>", False,0,1,1,0,"Opponent", -3, "青", "Opponent", "Any",
+Elder_BlueCounter1 = MoveClass(False,"追加介入<青黄>", False,0,1,1,0,"Opponent", -3, "青", "Opponent", "Any",
 0.3, False, 20, 0,0,0,0,0,0,0,0,0,0,0 )
 
 Elder_Counters = [Elder_RedCounter1, Elder_GreenCounter1, Elder_BlueCounter1]
 
-Elder_Reattack1 = MoveClass("再攻撃", False,1,0,0,0,"Self", -2, "赤", "Opponent", "Any",
+Elder_Reattack1 = MoveClass(False,"再攻撃", False,1,0,0,0,"Self", -2, "赤", "Opponent", "Any",
 0.3, True, 20, 0,0,0,0,0,0,0,0,0,0,0 )
 
 Elder_Reattacks = [Pig_Reattack1]
@@ -182,7 +422,7 @@ battleCount = 0
 winCount = 0
 drawCount = 0
 
-while (battleCount < 100):
+while (battleCount < 1):
     # setup, clean
     current_turn = 1
     for character in character_list:
